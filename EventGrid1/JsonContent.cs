94b56603e20c57577200a00e945cb33494b4688c
Shubham Gupta <shugupta@microsoft.com>
2019-08-19 17:18:07 +0530

    JsonContent.cs Added

---
+++ EventGrid1/JsonContent.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace EventGrid1
+{
+    public class JsonContent : StringContent
+    {
+        public JsonContent(object obj) :
+            base(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json")
+        { }
+    }
+}
